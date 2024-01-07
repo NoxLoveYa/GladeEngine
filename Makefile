71c41ac2f3601063fb170bcd714504dba11c757b
Ewan Massonet <ewan.massonet@epitech.eu>
2024-01-07 15:11:28 +0100

    [Add] Hello window

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
 CC = g++
 CFLAGS = -Wall -Wextra -std=c++17 
-LIBS = -lGL -lglfw
+LIBS = -lglfw3 -lGL -lX11
 
+GLAD_DIR = /usr/include/glad
 SRC_DIR = src
-SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
+SRC_FILES = $(wildcard $(GLAD_DIR)/glad.c $(SRC_DIR)/**.cpp)
 OBJ_DIR = obj
-OBJ_FILES = $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SRC_FILES))
+OBJ_FILES = $(patsubst wildcard $(GLAD_DIR)/glad.c $(SRC_DIR)/%%.cpp,$(OBJ_DIR)/%%.o,$(SRC_FILES))
 
 TARGET = GladeEngine
 
 $(TARGET): $(OBJ_FILES)
-	$(CC) $(CFLAGS) $^ -o $@
+	$(CC) $(CFLAGS) $^ -o $@ $(LIBS)
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
 	$(CC) $(CFLAGS) -c $< -o $@ $(LIBS)
@@ -21,6 +22,7 @@ clean:
 fclean: clean
 	rm -rf $(TARGET)
 
-test: $(TARGET) clean
-	clear
+re: fclean $(TARGET)
+
+test: re clean
 	./$(TARGET)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
-#include "stdio.h"
+#include <iostream>
+#include "headers/window.hpp"
 
 int main() {
     printf("Hello World!\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(window::WIN_WIDTH, window::WIN_HEIGHT, window::WIN_TITLE.c_str(), NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, window::WIN_WIDTH, window::WIN_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, window::framebuffer_size_callback); 
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
     return 0;
 }
